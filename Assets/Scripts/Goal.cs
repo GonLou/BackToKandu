9fdacfe8f43acd987f391f4f56c64447a5e721c0
GonLou <gnunodal@hotmail.com>
2016-01-31 12:27:06 +0000

    menu added

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -1,27 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Goal : MonoBehaviour {
+public class Goal : MonoBehaviour
+{
 
     public AudioSource drumSound;
 
     // Use this for initialization
-    void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     void OnCollisionEnter(Collision other)
     {
-        drumSound.Play();
+        playSoundThenLoad();
+
         if (other.gameObject.CompareTag("Player"))
         {
-            //Debug.Log("Hut");
             Application.LoadLevel(0);
         }
     }
+
+    IEnumerator playSoundThenLoad()
+    {
+        drumSound.Play();
+        yield return new WaitForSeconds(drumSound.clip.length);
+    }
+
 }
